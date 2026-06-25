eba616270d8cf81125c6e8636259a05c33871687
Olenka <olena.p008@gmail.com>
2026-06-25 21:36:11 +0300

    task resolved

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const formatted = sourceString
+    .split('\n')
+    .map((line) => line.trim())
+    .filter(Boolean);
+
+  for (const line of formatted) {
+    const [key, value] = line.split(':');
+
+    if (!key || !value) continue;
+
+    result[key.trim()] = value.trim();
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
