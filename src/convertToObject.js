1e6e91ed21073c9c2ce2976825863b96fe7f23b6
Olenka <olena.p008@gmail.com>
2026-06-25 21:38:20 +0300

    back to origin

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,23 +5,6 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  const result = {};
-
-  const formatted = sourceString
-    .split('\n')
-    .map((line) => line.trim())
-    .filter(Boolean);
-
-  for (const line of formatted) {
-    const [key, value] = line.split(':');
-
-    if (!key || !value) continue;
-
-    result[key.trim()] = value.trim();
-  }
-
-  return result;
-}
+function convertToObject(sourceString) {}
 
 module.exports = convertToObject;
